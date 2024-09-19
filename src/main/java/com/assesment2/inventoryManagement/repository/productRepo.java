2cab24ffec190bf4afb0d3a026c17715fddd4fa9
Manikandan <bnairmanikandan@gmail.com>
2024-09-19 09:54:10 +0530

    minor changes

--- src/main/java/com/assesment2/inventoryManagement/repository/productRepo.java
+++ src/main/java/com/assesment2/inventoryManagement/repository/productRepo.java
@@ -3,6 +3,6 @@ package com.assesment2.inventoryManagement.repository;
 import com.assesment2.inventoryManagement.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepo extends JpaRepository<Product,Integer> {
+public interface ProductRepo extends JpaRepository<Product, Integer> {
 
 }
